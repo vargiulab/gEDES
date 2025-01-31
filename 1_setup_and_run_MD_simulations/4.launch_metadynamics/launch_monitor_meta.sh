aae3c0c6102381e8d45656e1752c018aa6ba3b8b
Han Kurt <71261419+hankurt@users.noreply.github.com>
2025-01-31 13:15:13 +0100

    Update launch_monitor_meta.sh

--- 1_setup_and_run_MD_simulations/4.launch_metadynamics/launch_monitor_meta.sh
+++ 1_setup_and_run_MD_simulations/4.launch_metadynamics/launch_monitor_meta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run gmx_mpi command here
-mpirun -np 8 gmx_mpi mdrun -gpu_id 0123 -multidir rep0 rep1 rep2 rep3 rep4 rep5 rep6 rep7 -s topol -deffnm BE_AK -cpi BE_AK.cpt -plumed plumed-common.dat -dlb yes -replex 50000 -nsteps 275000000 
+mpirun -np 8 gmx_mpi mdrun -gpu_id 0123 -multidir rep0 rep1 rep2 rep3 rep4 rep5 rep6 rep7 -s topol -deffnm BE_AK -cpi BE_AK.cpt -plumed plumed.dat -dlb yes -replex 50000 -nsteps 275000000 
 
 # Check the exit status of gmx_mpi in a loop
 while true
@@ -9,7 +9,7 @@ do
     if ! ps ax | grep -v grep | grep gmx_mpi > /dev/null
     then
         # Launch relaunch-meta.sh when gmx_mpi stops
-	mpirun -np 8 --hostfile host gmx_mpi mdrun -gpu_id 0123 -multidir rep0 rep1 rep2 rep3 rep4 rep5 rep6 rep7 -s topol -deffnm BE_AK -append -cpi BE_AK.cpt -plumed plumed-common.dat -dlb yes -replex 50000 -nsteps 275000000
+	mpirun -np 8 --hostfile host gmx_mpi mdrun -gpu_id 0123 -multidir rep0 rep1 rep2 rep3 rep4 rep5 rep6 rep7 -s topol -deffnm BE_AK -append -cpi BE_AK.cpt -plumed plumed.dat -dlb yes -replex 50000 -nsteps 275000000
        # break
     fi
     sleep 30
